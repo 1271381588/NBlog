4b5a3891ab1968d894e0590722d858dc0ffe0d89
shuxiazi <1271381588@qq.com>
2021-08-20 20:29:59 +0800

    📃 docs(修改工信部链接):

--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'assets': '@/assets',
-				'common': '@/common',
-				'components': '@/components',
-				'api': '@/api',
-				'views': '@/views',
-				'plugins': '@/plugins'
-			}
-		}
-	},
-	publicPath:
-    process.env.NODE_ENV === "production"
-      ? `https://cdn.jsdelivr.net/gh/1271381588/NBlog@1.0.4/blog-view/dist/`
-      : "/"
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'api': '@/api',
+                'views': '@/views',
+                'plugins': '@/plugins'
+            }
+        }
+    },
+    publicPath: process.env.NODE_ENV === "production" ?
+        `https://cdn.jsdelivr.net/gh/1271381588/NBlog@1.0.6/blog-view/dist/` : "/"
 
 }
